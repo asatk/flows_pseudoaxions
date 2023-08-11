5ffeed0f326000223e2c5a6ec42e9133034c2ef8
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-08-10 23:40:37 -0400

    better loading for .ROOT files

--- utils/data.py
+++ utils/data.py
@@ -8,6 +8,8 @@ a normalizing flow.
 
 import numpy as np
 import os
+import re
+from typing import Callable
 import uproot as up
 
 from .constants import DEFAULT_SEED
@@ -28,6 +30,9 @@ ptstr = "Photon_pt"
 ptidxstr = "CBL_RecoPhi_photonindex"
 labelphistr = "GenPhi_mass"
 labelomegastr = "GenOmega_mass"
+expressions = [
+    phistr, omegastr, omegaidxstr, ptstr, ptidxstr, labelphistr, labelomegastr]
+
 
 def makedata(mode: int, load_data_path: str=None, save_data_path: str=None,
              use_whiten: bool=True, overwrite: bool=False, **kwargs) -> tuple[np.ndarray, np.ndarray]:
@@ -112,6 +117,115 @@ def makedata(mode: int, load_data_path: str=None, save_data_path: str=None,
                    overwrite=overwrite)
     
     return data, cond
+
+
+def _find_root(data_dir, max_depth: int=3):
+    """
+    Locate the paths to all of the .ROOT files in a given directory. This
+    function is recursive and will only descend the number of subtrees as given
+    by `max_depth`.
+    """
+    p = re.compile(".+\.ROOT$", re.IGNORECASE)
+    file_list = []
+    with os.scandir(data_dir) as d:
+        for entry in d:
+            if entry.is_dir():
+                if max_depth <= 0:
+                    print_msg("Maximum recursion depth reached.", level=LOG_WARN)
+                    return file_list
+                else:
+                    list_subtree = _find_root(data_dir + "/" + entry.name, max_depth=max_depth-1)
+                    file_list.extend(list_subtree)
+            elif p.match(entry.name) is not None:
+                path = data_dir + "/" + entry.name
+                if os.stat(path).st_size == 0:
+                    print_msg(f"--- {path}: empty file ---", level=LOG_WARN)
+                    return []
+                return [path + ":Events;1"]
+            
+    return file_list
+
+
+def _evt_sel_1(arr: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    
+    # cut out events that don't have a valid omega/pt to index
+    omegaidxarr = arr[omegaidxstr]
+    ptidxarr = arr[ptidxstr]
+
+    # -1 & -1 == -1 only way to get -1 b/c -1 is all 1 bits
+    # idxcutarr = ((omegaidxarr | ptidxarr) != -1)
+    idxcutarr = ((omegaidxarr & ptidxarr) != -1)
+
+    # select the events with valid indexes for each variable
+    phi = arr[phistr][idxcutarr]
+    omega = arr[omegastr][idxcutarr]
+    omegaidx = arr[omegaidxstr][idxcutarr]
+    pt = arr[ptstr][idxcutarr]
+    ptidx = arr[ptidxstr][idxcutarr]
+    labelphi = arr[labelphistr][idxcutarr]
+    labelomega = arr[labelomegastr][idxcutarr]
+
+    # arrays to store indexed data that satisfy the cut
+    omega_temp = np.empty_like(omega, dtype=np.float32)
+    labelphi_temp = np.empty_like(labelphi, dtype=np.float32)
+    labelomega_temp = np.empty_like(labelomega, dtype=np.float32)
+
+    # perform cut and extract correct element with index
+    for i in range(len(omega)):
+        if pt[i][ptidx[i]] > 220:
+            omega_temp[i] = omega[i][omegaidx[i]]
+            labelphi_temp[i] = labelphi[i][0]
+            labelomega_temp[i] = labelomega[i][0]
+        else:
+            omega_temp[i] = np.nan
+
+    # copy only the events that satisfy the cut
+    cutarr = np.isfinite(omega_temp)
+    newphi = phi[cutarr].copy()
+    newomega = omega_temp[cutarr].copy()
+    newlabelphi = labelphi_temp[cutarr].copy()
+    newlabelomega = labelomega_temp[cutarr].copy()
+
+    samples = np.stack((newphi, newomega), axis=1)
+    labels = np.stack((newlabelphi, newlabelomega), axis=1)
+
+    return samples, labels
+
+
+def _load_root(root_paths, event_selection_fn: Callable[[np.ndarray],
+        tuple[np.ndarray]], expressions: list[str], cutstr: str,
+        event_thresh: int=100):
+    """
+    Load events from all provided .ROOT files using a given event selection
+    scheme. 
+    """
+        
+    num_workers = 4
+    arr = up.concatenate(root_paths, expressions=expressions, cut=cutstr,
+        num_workers=num_workers, begin_chunk_size="2 MB", library="np")
+
+    samples_temp, labels_temp = event_selection_fn(arr,
+        expressions=expressions, cutstr=cutstr)
+
+    # separate samples/labels into groups per label
+    labels_unique, inverse_unique = np.unique(labels_temp, return_inverse=True,
+        axis=0)
+    samples_grp = [
+        samples_temp[inverse_unique == i] for i in range(len(labels_unique))
+    ]
+
+    # compile samples and labels array
+    samples = np.zeros(shape=(0, samples_temp.shape[-1]))
+    labels = np.zeros(shape=(0, labels_temp.shape[-1]))
+
+    for i, sample_i in enumerate(samples_grp):
+        label_i = labels_unique[i]
+        # only include labels with sufficiently many statistics after cuts
+        if len(sample_i) >= event_thresh:
+            samples = np.r_[samples, sample_i]
+            labels = np.r_[labels, [label_i]]
+
+    return samples, labels
 
 
 def _loadallroot(data_dir: str, event_threshold: float=0.01) -> np.ndarray:
