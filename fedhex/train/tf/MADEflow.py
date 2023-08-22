de084770e4a44926f183e6f14f76ba90c6639d50
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-08-22 12:28:47 -0500

    MADEManager cleanup

--- fedhex/train/tf/MADEflow.py
+++ fedhex/train/tf/MADEflow.py
@@ -21,15 +21,22 @@ from tensorflow_probability.python.bijectors import MaskedAutoregressiveFlow as 
 from typing import Any
 import numpy as np
 
-from ...io._path import LOG_WARN, LOG_FATAL, print_msg, save_config
+from ...io._path import LOG_WARN, LOG_ERROR, LOG_FATAL, print_msg, save_config
 from ..tf._train import train
 
 
 class MADEManager():
+    """
+    The details of building and training a model are self-contained within
+    this class.
+    """
     def __init__(self, nmade: int, ninputs: int, ncinputs: int,
                  hidden_layers: int, hidden_units: int,
                  lr_tuple: tuple[int]) -> None:
         
+        self._is_compiled = False
+        self._is_trained = False
+
         self._nmade = nmade
         self._ninputs = ninputs
         self._ncinputs = ncinputs
@@ -38,6 +45,11 @@ class MADEManager():
         self._lr_tuple = lr_tuple
 
     def compile_model(self) -> None:
+        """
+        Compile a model with all of the necessary parameters. This Manager will
+        keep references to instances of tf.Model, tfd.TransformedDistribution,
+        and a list of MADE blocks, all used internally.
+        """
         model, dist, made_list = compile_MADE_model(num_made=self._nmade,
             num_inputs=self._ninputs, num_cond_inputs=self._ncinputs,
             hidden_layers=self._hidden_layers, hidden_units=self._hidden_units,
@@ -46,10 +58,20 @@ class MADEManager():
         self._model = model
         self._dist = dist
         self._made_list = made_list
+        self._is_compiled = True
 
     def train_model(self, data, cond, nepochs, batch_size, starting_epoch=0,
                     flow_path: str|None=None, callbacks: list=None) -> None:
-        
+        """
+        Train the model once built.
+        """
+
+        if self._is_compiled is False:
+            print_msg("The model is not compiled. Please use the instance " + \
+                      "method `MADEManager.compile_model()` in order for " + \
+                      "this model to be trainable.", level=LOG_ERROR)
+            return
+
         if callbacks == None:
             callbacks = []
 
@@ -61,8 +83,15 @@ class MADEManager():
         train(self._model, data, cond, nepochs=nepochs, batch_size=batch_size,
               starting_epoch=starting_epoch, flow_path=flow_path,
               callbacks=callbacks)
+        self._is_trained = True
         
     def eval_model(self, cond) -> np.ndarray:
+
+        if self._is_trained is False:
+            print_msg("The model is not train. Please use the instance " + \
+                      "method `MADEManager.train_model()` in order for " + \
+                      "this model to be evaluatable.", level=LOG_ERROR)
+            return None
         
         # Generate samples
         current_kwargs = {}
@@ -72,10 +101,10 @@ class MADEManager():
         gen_data = self._dist.sample(len(cond), bijector_kwargs=current_kwargs)
         return gen_data
     
-    def save_model(self, flow_path: str):
+    def save_model(self, flow_path: str) -> None:
         self._model.save(flow_path)
 
-    def save(self, config_path: str):
+    def save(self, config_path: str) -> None:
         d = {"nmade": self._nmade, "ninputs": self._ninputs,
              "ncinputs": self._ncinputs, "hidden_layers": self._hidden_layers,
              "hidden_units": self._hidden_units, "lr_tuple": self._lr_tuple,
